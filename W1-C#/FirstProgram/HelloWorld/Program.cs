14f26484d0c4738923b93c89aaa4b94eaca29732
Soulzeon <79062042+Soulzeon@users.noreply.github.com>
2021-02-15 19:41:23 -0700

    added first program

---
+++ W1-C#/FirstProgram/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+            
+            int[] numbers = {1,2,3,4,5};
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+        }
+    }
+}
